d299e204c89029f80637999786f8bd692616a6a5
Armands Strazds <strazds@gmail.com>
2024-08-04 20:30:03 +0200

    refactor model

--- demo/src/main/java/com/example/demo/controller/HomeController.java
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,22 +1,23 @@
 package com.example.demo.controller;
 
-import com.example.demo.service.HomeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.model.HomeModel;
+
 @Controller
 public class HomeController {
 
-    private final HomeService homeService;
+    private final HomeModel homeModel;
 
-    public HomeController(HomeService homeService) {
-        this.homeService = homeService;
+    public HomeController(HomeModel homeModel) {
+        this.homeModel = homeModel;
     }
 
     @GetMapping("/")
     public String index(Model model) {
-        homeService.prepareModel(model);
+        homeModel.prepareModel(model);
         return "index";
     }
 }
--- demo/src/main/java/com/example/demo/model/HomeModel.java
+++ demo/src/main/java/com/example/demo/model/HomeModel.java
@@ -1,10 +1,10 @@
-package com.example.demo.service;
+package com.example.demo.model;
 
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
 @Service
-public class HomeService {
+public class HomeModel {
 
     public void prepareModel(Model homeModel) {
         homeModel.addAttribute("title", "Thymeleaf Fetch Example");
